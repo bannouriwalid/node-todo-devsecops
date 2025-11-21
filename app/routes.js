e3c59fd6af60d64b16e00c4adfa44b08f44d1b23
Walid Bannouri <bannouri.walid1@gmail.com>
2025-11-21 20:48:37 +0100

    add fixes

--- app/routes.js
+++ app/routes.js
@@ -1,57 +1,47 @@
-var Todo = require('./models/todo');
+const Todo = require('./models/todo');
 
-function getTodos(res) {
-    Todo.find(function (err, todos) {
-
-        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-        if (err) {
-            res.send(err);
-        }
-
+async function getTodos(res) {
+    try {
+        const todos = await Todo.find();
         res.json(todos); // return all todos in JSON format
-    });
-};
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
 
 module.exports = function (app) {
 
     // api ---------------------------------------------------------------------
     // get all todos
-    app.get('/api/todos', function (req, res) {
-        // use mongoose to get all todos in the database
-        getTodos(res);
+    app.get('/api/todos', async (req, res) => {
+        await getTodos(res);
     });
 
     // create todo and send back all todos after creation
-    app.post('/api/todos', function (req, res) {
-
-        // create a todo, information comes from AJAX request from Angular
-        Todo.create({
-            text: req.body.text,
-            done: false
-        }, function (err, todo) {
-            if (err)
-                res.send(err);
-
-            // get and return all the todos after you create another
-            getTodos(res);
-        });
-
+    app.post('/api/todos', async (req, res) => {
+        try {
+            await Todo.create({
+                text: req.body.text,
+                done: false
+            });
+            await getTodos(res); // return updated list
+        } catch (err) {
+            res.status(500).json({ error: err.message });
+        }
     });
 
     // delete a todo
-    app.delete('/api/todos/:todo_id', function (req, res) {
-        Todo.remove({
-            _id: req.params.todo_id
-        }, function (err, todo) {
-            if (err)
-                res.send(err);
-
-            getTodos(res);
-        });
+    app.delete('/api/todos/:todo_id', async (req, res) => {
+        try {
+            await Todo.deleteOne({ _id: req.params.todo_id });
+            await getTodos(res); // return updated list
+        } catch (err) {
+            res.status(500).json({ error: err.message });
+        }
     });
 
     // application -------------------------------------------------------------
-    app.get('*', function (req, res) {
-        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    app.get('*', (req, res) => {
+        res.sendFile(__dirname + '/public/index.html'); // load the single view file
     });
 };
